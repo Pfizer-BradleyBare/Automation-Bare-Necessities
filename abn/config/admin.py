9801d3b129d0cd119c05553b091997bcde2e899a
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-07-23 14:41:17 -0500

    Automatically create an admin user if one does not exist.

--- abn/config/admin.py
+++ abn/config/admin.py
@@ -1,5 +1,6 @@
 from django.contrib.admin import AdminSite, site
 from django.contrib.auth.models import Group, User
+from django.db.utils import OperationalError
 
 
 class ConfigAdmin(AdminSite): ...
@@ -12,6 +13,7 @@ config_admin = ConfigAdmin(name="config_admin")
 # Register your models here.
 
 
+# This disables the login page for admin.
 class AccessUser:
     has_module_perms = has_perm = __getattr__ = lambda s, *a, **kw: True
 
@@ -24,3 +26,13 @@ site.unregister(Group)
 site.unregister(User)
 
 site.has_permission = lambda r: setattr(r, "user", AccessUser()) or True  # type:ignore
+
+# must have at least 1 user to save things to the DB
+try:
+    if len(User.objects.all()) == 0:
+
+        user = User(username="admin", password="admin")
+
+        user.save()
+except OperationalError:
+    ...
