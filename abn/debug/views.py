34d0a1a982f76926c4988e294ed69eba55d1b82f
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-07-22 20:46:40 -0500

    Incorporated htmx for better filtering

--- abn/debug/views.py
+++ abn/debug/views.py
@@ -5,23 +5,57 @@ from .models import TraceEntry
 
 
 def trace(request: HttpRequest):
-    objects = list(TraceEntry.objects.all())
 
-    context = {
-        "rows": sorted(
-            [
+    num_objects = 500
+
+    if request.method == "POST":
+        log_level = request.POST["input-log-level"]
+        method_name = request.POST["input-method-name"]
+        device_identifier = request.POST["input-device-identifier"]
+        debug_message = request.POST["input-debug-message"]
+
+        query = TraceEntry.objects
+        if log_level != "ALL":
+            query = query.filter(level=log_level)
+        if method_name != "":
+            query = query.filter(method__name__icontains=method_name)
+        if device_identifier != "":
+            query = query.filter(device_identifier__icontains=device_identifier)
+        if debug_message != "":
+            query = query.filter(message__icontains=debug_message)
+
+        objects = query.all()[:num_objects]
+        context = {
+            "rows": sorted(
                 [
-                    object.time_stamp.strftime("%b %d, %Y, %I:%M %p"),
-                    object.level,
-                    object.method.name,
-                    object.device_identifier,
-                    object.message,
-                ]
-                for object in objects
-            ]
-            * 500,
-            key=lambda x: x[0],
-        ),
-    }
-
-    return render(request, "debug/trace.html", context)
+                    [
+                        object.time_stamp.strftime("%b %d, %Y, %I:%M %p"),
+                        object.level,
+                        object.method.name,
+                        object.device_identifier,
+                        object.message,
+                    ]
+                    for object in objects
+                ],
+                key=lambda x: x[0],
+            ),
+        }
+        return render(request, "debug/trace_body.html", context)
+    else:
+        objects = TraceEntry.objects.all()[:num_objects]
+        context = {
+            "rows": sorted(
+                [
+                    [
+                        object.time_stamp.strftime("%b %d, %Y, %I:%M %p"),
+                        object.level,
+                        object.method.name,
+                        object.device_identifier,
+                        object.message,
+                    ]
+                    for object in objects
+                ],
+                key=lambda x: x[0],
+            ),
+        }
+        return render(request, "debug/trace.html", context)
