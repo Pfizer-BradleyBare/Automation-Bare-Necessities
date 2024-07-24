d1f1b8c57f90916c88879405f6740d079b876c0f
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-07-24 16:55:29 -0500

    To continue tomorrow

--- abn/method/views/test_index.py
+++ abn/method/views/test_index.py
@@ -1,22 +1,35 @@
 from django.http import HttpRequest
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 
 from abn.views import NavbarView
 
 
 class TestIndexView(NavbarView):
     def get_context_data(self, **kwargs) -> dict:
-        return super().get_context_data(**kwargs) | {
+        kwargs.setdefault("request", None)
+
+        body_context = {}
+
+        body = render_to_string(
+            "method/test_submit.html",
+            body_context,
+            kwargs.get("request"),
+        )
+
+        additional_context = {
             "header": "Test Method",
             "description": 'Will test an ABN method containing both a "Method" and a "Worklist" sheet. Once testing completes successfully you will be able to download a preparation list and queue the method for execution.',
-            "body": "",
+            "body": body,
         }
+
+        return super().get_context_data() | additional_context
 
     def get(self, request: HttpRequest):
         return render(
             request,
-            "method/test_index.html",
-            self.get_context_data(),
+            "method/test_base.html",
+            self.get_context_data(request=request),
         )
 
     def post(self, request: HttpRequest):
--- abn/method/views/test_progress.py
+++ abn/method/views/test_progress.py
@@ -1,13 +1,39 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpRequest
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
 from abn.views import NavbarView
 
 
 class TestProgressView(NavbarView):
+    def get_context_data(self, **kwargs) -> dict:
+        kwargs.setdefault("request", None)
+
+        filename = kwargs.get("filename")
+
+        try:
+            body_context = {}
+        except ObjectDoesNotExist:
+            body_context = {}
+
+        body = render_to_string(
+            "method/test_progress.html",
+            body_context,
+            kwargs.get("request"),
+        )
+
+        additional_context = {
+            "header": "Test Method",
+            "description": 'Will test an ABN method containing both a "Method" and a "Worklist" sheet. Once testing completes successfully you will be able to download a preparation list and queue the method for execution.',
+            "body": body,
+        }
+
+        return super().get_context_data() | additional_context
+
     def get(self, request: HttpRequest, filename: str):
         return render(
             request,
-            "method/test_index.html",
-            self.get_context_data(),
+            "method/test_base.html",
+            self.get_context_data(request=request, filename=filename),
         )
