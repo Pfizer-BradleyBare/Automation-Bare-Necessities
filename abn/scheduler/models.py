04625af6401e29a344b36cdc9327c237bdc137d3
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-07-25 18:57:22 -0500

    add fields to scheduler model

--- abn/scheduler/models.py
+++ abn/scheduler/models.py
@@ -4,9 +4,11 @@ from django.db import models
 
 
 def get_upload_path(instance, filename):
-    print(filename[:-5])
     return f"_db_files/queued_methods/{filename[:-5]}/{datetime.now().strftime('%Y%m%d%H%M%S')}_{filename}"
 
 
 class QueuedMethod(models.Model):
-    file = models.FileField(upload_to=get_upload_path, max_length=255)
+    emails = models.CharField(max_length=100)
+    phone_numbers = models.CharField(max_length=100)
+    completion_time = models.DateTimeField()
+    method_file = models.FileField(upload_to=get_upload_path, max_length=255)
