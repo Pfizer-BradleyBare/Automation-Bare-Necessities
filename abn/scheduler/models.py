8feb4b7533695004b56db77022611e0bf08537e9
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-07-25 16:15:32 -0500

    update path with time to prevent overwrites, but to save full history

--- abn/scheduler/models.py
+++ abn/scheduler/models.py
@@ -1,10 +1,12 @@
+from datetime import datetime
+
 from django.db import models
 
 
 def get_upload_path(instance, filename):
     print(filename[:-5])
-    return f"_db_files/queued_methods/{filename[:-5]}/{filename}"
+    return f"_db_files/queued_methods/{filename[:-5]}/{datetime.now().strftime('%Y%m%d%H%M%S')}_{filename}"
 
 
 class QueuedMethod(models.Model):
-    file = models.FileField(upload_to=get_upload_path)
+    file = models.FileField(upload_to=get_upload_path, max_length=255)
