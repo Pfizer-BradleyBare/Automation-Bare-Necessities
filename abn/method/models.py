b0db287811548bde37f4ec6942036823af3e2b7e
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-07-29 17:46:42 -0500

    ...

--- abn/method/models.py
+++ abn/method/models.py
@@ -5,12 +5,10 @@ from django.db import models
 
 
 def get_upload_path(instance, filename):
-    print(filename)
-    return f"_db_files/test_methods/{filename[:-5]}/method.xlsm"
+    return f"_db_files/test_methods/{filename}"
 
 
 class TestMethod(models.Model):
-
     filename = models.CharField(max_length=255)
     file = models.FileField(
         upload_to=get_upload_path,
@@ -23,6 +21,9 @@ class TestMethod(models.Model):
     def delete(self, using=None, keep_parents=False) -> tuple[int, dict[str, int]]:
         shutil.rmtree(Path(self.file.path).parent, ignore_errors=True)
         return super().delete(using, keep_parents)
+
+    def __str__(self) -> str:
+        return self.filename
 
 
 class TestMessage(models.Model):
