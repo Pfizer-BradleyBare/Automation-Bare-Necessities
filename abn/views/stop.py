c9e3149c1ebb5121108f189f0c9dc2b6c16e0565
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-08-02 12:41:34 -0500

    Add starting state.

--- abn/views/stop.py
+++ abn/views/stop.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from django.http import HttpRequest
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 import abn
 
@@ -10,5 +10,8 @@ from .navbar import NavbarView
 
 class StopView(NavbarView):
     def get(self, request: HttpRequest):
+        return render(request, "abn/stop.html", self.get_context_data())
+
+    def post(self, request: HttpRequest):
         abn.state = "stop"
         return redirect("abn:index")
