76f86f4e5f465d1f83b3e1865451664da0d535d9
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-09-25 12:12:17 -0500

    remove folder so I can add to gitignore

--- method/apps.py
+++ method/apps.py
@@ -1,6 +1,15 @@
+import shutil
+from pathlib import Path
+
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class MethodConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "method"
+
+    def ready(self) -> None:
+        path = Path(settings.BASE_DIR) / "_abn_methods"
+
+        shutil.rmtree(path, ignore_errors=True)
