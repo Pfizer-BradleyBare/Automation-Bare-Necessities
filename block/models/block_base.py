787167d83c566e2df3d56252be57623519371904
Bradley Bare <Bradley.Bare@pfizer.com>
2024-10-16 16:26:15 -0500

    Validation in progress.

--- block/models/block_base.py
+++ block/models/block_base.py
@@ -9,6 +9,7 @@ from polymorphic.models import PolymorphicModel
 
 from excel.definitions import BlockDefinitionExcelDefinition
 from method.models import MethodWorkbookBase
+from plh_config.labware.models import PipettableLabware
 
 DROPDOWN_CONTAINER_NAMES = "%%get_container_names_as_string"
 DROPDOWN_PREFIXED_CONTAINER_NAMES = "%%get_prefixed_container_names_as_string"
@@ -111,22 +112,92 @@ class BlockBase(PolymorphicModel):
         for parameter in definition.parameters:
             key_name = parameter.label
             field_name = parameter.block_field_name
+            advanced = parameter.advanced
 
             value = None
             try:
                 value = kwargs.pop(key_name)
             except KeyError:
-                self.is_valid = False
-                bound_logger.critical(
-                    f"{key_name} is missing from the block parameters",  # noqa: G004
-                )
+                if advanced is False:
+                    self.is_valid = False
+                    bound_logger.critical(
+                        f"{key_name} is missing from the block parameters",  # noqa: G004
+                    )
 
             setattr(self, field_name, value)
 
-    def validate_parameters(self): ...
+    def validate_parameters(self):
+        definition = self.get_excel_definition()
+
+        bound_logger = logger.bind(
+            source="ABN",
+            method=str(self.method),
+            row=self.row,
+            column=self.column,
+            block=type(self).__name__,
+        )
+
+        for parameter in definition.parameters:
+            field_name = parameter.block_field_name
+            field_type = parameter.block_field_type
+            free_text = parameter.free_text
+            dropdown_items = parameter.dropdown_items.split(",")
+
+            value = getattr(self, field_name)
+
+            if value is None:
+                continue
+            # This is a missing parameter. User notification is performed when we assign parameters. Let's not repeat it.
+
+            for dropdown_item in dropdown_items:
+                if dropdown_item in {
+                    DROPDOWN_CONTAINER_NAMES,
+                    DROPDOWN_PREFIXED_CONTAINER_NAMES,
+                }:
+                    from .pathways import ActivateContainer
+
+                    prefix = "_CONTAINER: "
+                    value = value.replace(prefix, "")
+
+                    if ActivateContainer.objects.filter(
+                        method=self.method,
+                        name=value,
+                    ).exists():
+                        continue
+
+                if dropdown_item in {
+                    DROPDOWN_CONTAINER_LABWARE_NAMES,
+                    DROPDOWN_PREFIXED_CONTAINER_LABWARE_NAMES,
+                }:
+                    prefix = ""
+                    value = value.replace(prefix, "")
+
+                    if PipettableLabware.objects.filter(identifier=value).exists():
+                        continue
+
+                if dropdown_item in {
+                    DROPDOWN_PREDEFINED_SOLUTION_NAMES,
+                    DROPDOWN_PREFIXED_PREDEFINED_SOLUTION_NAMES,
+                }:
+                    prefix = ""
+                    value = value.replace(prefix, "")
+
+                if dropdown_item in {
+                    DROPDOWN_USER_DEFINED_SOLUTION_NAMES,
+                    DROPDOWN_PREFIXED_USER_DEFINED_SOLUTION_NAMES,
+                }:
+                    prefix = ""
+                    value = value.replace(prefix, "")
+
+                if dropdown_item in {
+                    DROPDOWN_WORKLIST_COLUMN_NAMES,
+                    DROPDOWN_PREFIXED_WORKLIST_COLUMN_NAMES,
+                }:
+                    prefix = "_WORKLIST: "
+                    value = value.replace(prefix, "")
 
     def __init_subclass__(cls: type[BlockBase]) -> None:
         cls.block_subclasses[cls.__name__] = cls
 
     def __str__(self) -> str:
-        return f"{self.method}: {self.row+1}|{self.column+3} -> {type(self).__name__} "
+        return f"{self.method}: {self.row}|{self.column} -> {type(self).__name__} "
