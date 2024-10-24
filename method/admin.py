9ee7a0b847823d0f0efa72b6db5f6e369752de45
Bradley Bare <Bradley.Bare@pfizer.com>
2024-10-24 14:54:50 -0500

    Make method file not editable in admin after creation

--- method/admin.py
+++ method/admin.py
@@ -1,4 +1,9 @@
+from __future__ import annotations
+
+from typing import Any
+
 from django.contrib import admin
+from django.http import HttpRequest
 
 from .models import (
     ExecutingMethodWorkbook,
@@ -6,6 +11,21 @@ from .models import (
     TestingMethodWorkbook,
 )
 
-admin.site.register(TestingMethodWorkbook)
-admin.site.register(ExecutingMethodWorkbook)
-admin.site.register(TemplateMethodWorkbook)
+
+class WorkbookAdmin(admin.ModelAdmin):
+    def get_readonly_fields(
+        self,
+        _: HttpRequest,
+        obj: Any | None = ...,
+    ) -> list[str] | tuple[Any, ...]:
+        if obj:
+            return ["file"]
+
+        return []
+
+    # Disable being able to change the file after method is created. Protects from major issues down the line caused by changing the method file in admin.
+
+
+admin.site.register(TestingMethodWorkbook, WorkbookAdmin)
+admin.site.register(ExecutingMethodWorkbook, WorkbookAdmin)
+admin.site.register(TemplateMethodWorkbook, WorkbookAdmin)
