e5915fe4cbc107d2f5a6ed21ad9befc33dfd429f
Bradley <bradley.bare@pfizer.com>
2024-10-31 15:51:49 -0500

    Playing with deck models. Not really sure how to proceed yet.

---
+++ deck/models/loaded_labware/loaded_labware.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from plh_config.labware.models import LabwareBase
+from plh_config.layout_item.models import LayoutItemBase
+
+
+class LoadedLabware(models.Model):
+    labware = models.ForeignKey(to=LabwareBase, on_delete=models.CASCADE)
+
+    layout_item = models.ForeignKey(to=LayoutItemBase, on_delete=models.CASCADE)
