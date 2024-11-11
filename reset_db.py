fa57f93b0b7370ddbdf06ea6c104ccb806817dfa
Bradley <bradley.bare@pfizer.com>
2024-11-11 15:46:28 -0600

    Updated to automatically migrate and load initial_data fixtures,

--- reset_db.py
+++ reset_db.py
@@ -9,3 +9,15 @@ for root, dirs, files in os.walk(pathlib.Path(__file__).parent):
         for file in files:
             if ".py" in file and "__init__.py" not in file:
                 (pathlib.Path(root) / file).unlink(True)
+
+os.system("python manage.py makemigrations")
+os.system("python manage.py migrate")
+
+fixtures = []
+
+for root, dirs, files in os.walk(pathlib.Path(__file__).parent):
+    for file in files:
+        if "initial_data" in file:
+            fixtures.append(file)
+
+os.system(f"python manage.py loaddata {" ".join(fixtures)}")
