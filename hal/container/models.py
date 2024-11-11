c4fd1bb997a6140195c31d2e1b702732c2bbe2b7
Bradley <bradley.bare@pfizer.com>
2024-11-11 10:09:30 -0600

    Change identifier to name. Add default for JSON field. Add clean method for 'shape_definition"

--- hal/container/models.py
+++ hal/container/models.py
@@ -2,17 +2,14 @@ from __future__ import annotations
 
 from itertools import pairwise
 
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
 class Container(models.Model):
-    class Meta:
-        ordering = ["identifier"]
-
-    identifier = models.CharField(
+    name = models.CharField(
         max_length=255,
         unique=True,
-        editable=False,
         blank=False,
         null=False,
     )
@@ -27,9 +24,57 @@ class Container(models.Model):
         help_text="Minimum volume required in a container for successful aspiration.",
     )
 
-    shape_definition = models.JSONField(
-        help_text="A list of (volume uL, height mm) pairs that define the shape of a container. Pipetting accurately in a container requires an accurate container shape definition.",
+    shape_definition: models.JSONField[list[tuple[float, float]]] = models.JSONField(
+        help_text="A list of [volume uL, height mm] pairs, in volume order, that define the shape of a container. Pipetting accurately in a container requires an accurate container shape definition.",
+        default=[
+            ("Volume 1", "Height 1"),
+            ("Volume 2", "Height 2"),
+            ("Volume ...", "Height ..."),
+        ],
     )
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+    def clean(self) -> None:
+        defintion = self.shape_definition
+
+        try:
+            for index, (volume, height) in enumerate(defintion):
+                if len(defintion[index]) != 2:
+                    raise ValidationError(
+                        "'Shape definition' can only contain pairwise number values in volume order. Ex: [[0,0],[1,1],[10,10]]",
+                    )
+
+                defintion[index] = (float(volume), float(height))
+        except ValueError as e:
+            raise ValidationError(
+                "'Shape definition' can only contain pairwise number values in volume order. Ex: [[0,0],[1,1],[10,10]]",
+            ) from e
+
+        defintion = sorted(defintion, key=lambda x: x[0])
+
+        volume, height = defintion[0]
+
+        if volume != 0 and height != 0:
+            defintion.insert(0, (0, 0))
+
+        pairs = pairwise(defintion)
+
+        self.shape_definition = defintion
+
+        for index, ((v1, h1), (v2, h2)) in enumerate(pairs):
+            if h1 > h2 or v1 > v2:
+                raise ValidationError(
+                    f"'Shape definition' does not make sense. Entry #{index+1} is has values greater than entry #{index+2}",
+                )
+            if h1 == h2 or v1 == v2:
+                raise ValidationError(
+                    f"'Shape definition' does not make sense. Entry #{index+1} is has values equal to entry #{index+2}",
+                )
 
     def interpolate_volume(self: Container, volume: float) -> float:
         """Calculate height at a given volume."""
@@ -114,6 +159,3 @@ class Container(models.Model):
         b = v1
 
         return m * x + b
-
-    def __str__(self) -> str:
-        return self.identifier
