3ad59081e25c523ca08c6f234535bed7a9ce37bb
Bradley <bradley.bare@pfizer.com>
2024-11-12 15:05:40 -0600

    help text

--- hal/deck/models/deck_base.py
+++ hal/deck/models/deck_base.py
@@ -3,7 +3,11 @@ from polymorphic.models import PolymorphicModel
 
 
 class DeckBase(PolymorphicModel):
-    identifier = models.CharField(max_length=255, unique=True)
+    identifier = models.CharField(
+        max_length=255,
+        unique=True,
+        help_text="Unique name to distinguish this deck from other decks.",
+    )
 
     class Meta:
         ordering = ["identifier"]
--- hal/deck/models/sub_deck.py
+++ hal/deck/models/sub_deck.py
@@ -5,7 +5,11 @@ from .deck_base import DeckBase
 
 
 class SubDeck(DeckBase):
-    parent_deck = models.ForeignKey(to=Deck, on_delete=models.CASCADE)
+    parent_deck = models.ForeignKey(
+        to=Deck,
+        on_delete=models.CASCADE,
+        help_text="Deck on which this sub deck is located.",
+    )
 
     def __str__(self) -> str:
         return f"{self.parent_deck.identifier} | {self.identifier}"
