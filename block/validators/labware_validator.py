f7a91f536a0624eff59ff4aaafe2486832807799
Bradley <bradley.bare@pfizer.com>
2024-11-12 11:48:52 -0600

    Updated to validate labware based on containers that are part of the deck configuration

--- block/validators/labware_validator.py
+++ block/validators/labware_validator.py
@@ -1,13 +1,16 @@
 from typing import Any
 
+from hal.labware.models import PipettableLabware
+from hal.layout_item.models import LayoutItemBase
 from method.models import MethodWorkbookBase
-
-# from hal.labware.models import LabwareBase
 
 
 def labware_validator(value: Any, _: MethodWorkbookBase) -> bool:
     """Check if the value is a labware identifier for this systems configuration. Does not expect extra kwargs."""
-    return True
-    # return LabwareBase.objects.filter(
-    #    identifier=value.replace(PREFIX_CONTAINER_LABWARE_NAMES, ""),
-    # ).exists()
+    from ..models.block_base import PREFIX_CONTAINER_LABWARE_NAMES
+
+    return value.replace(PREFIX_CONTAINER_LABWARE_NAMES, "") in {
+        layout_item.labware.container.identifier
+        for layout_item in LayoutItemBase.objects.all()
+        if isinstance(layout_item.labware, PipettableLabware)
+    }
