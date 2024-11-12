49774bb3b904d1858d5913420189a551223fadaf
Bradley <bradley.bare@pfizer.com>
2024-11-12 11:29:33 -0600

    Improve str method

--- hal/deck/models/sub_deck.py
+++ hal/deck/models/sub_deck.py
@@ -6,3 +6,6 @@ from .deck_base import DeckBase
 
 class SubDeck(DeckBase):
     parent_deck = models.ForeignKey(to=Deck, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.parent_deck.identifier} | {self.identifier}"
