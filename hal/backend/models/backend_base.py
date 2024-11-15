c40313b9c2892788d9d5f621d729150d3624dc76
Bradley Bare <Bradley.Bare@pfizer.com>
2024-11-15 11:08:55 -0600

    Added init and deinit to backend class as abstract methods.

--- hal/backend/models/backend_base.py
+++ hal/backend/models/backend_base.py
@@ -1,10 +1,16 @@
+from abc import abstractmethod
+from typing import ClassVar
+
+from django.core.exceptions import ValidationError
 from django.db import models
-from polymorphic.models import PolymorphicModel
+from plh.tools import BackendBase as PLHBackendBase
+
+from abstract.models import AbstractPolymorphicModel
+from debug import plh_logger
 
 
-class BackendBase(PolymorphicModel):
-    class Meta:
-        ordering = ["identifier"]
+class BackendBase(AbstractPolymorphicModel):
+    _backend_instances: ClassVar[dict[str, PLHBackendBase]] = {}
 
     identifier = models.CharField(
         max_length=50,
@@ -13,5 +19,25 @@ class BackendBase(PolymorphicModel):
         null=False,
     )
 
+    @abstractmethod
+    def initialize(self): ...
+
+    @abstractmethod
+    def deinitialize(self): ...
+
+    def clean(self) -> None:
+        if self.identifier in self._backend_instances:
+            plh_logger.critical(
+                f"Backend '{self.identifier}' is currently running. To make changes you first need to stop the backend.",
+            )
+            raise ValidationError(
+                "Backend '{self.identifier}' is currently running. To make changes you first need to stop the backend.",
+            )
+
+        return super().clean()
+
     def __str__(self) -> str:
         return self.identifier
+
+    class Meta:
+        ordering = ["identifier"]
