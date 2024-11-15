ce8246f628cbd93baac634400a6c59d5a20ef028
Bradley Bare <Bradley.Bare@pfizer.com>
2024-11-15 11:11:27 -0600

    Implemented abstract methods for backend

--- hal/backend/models/hamilton/microlab_star.py
+++ hal/backend/models/hamilton/microlab_star.py
@@ -1,4 +1,37 @@
+from pathlib import Path
+
+from plh.hamilton_venus.backend import MicrolabSTAR as PLHMicrolabSTAR
+
+from debug import plh_logger
+
 from .hamilton_backend_base import HamiltonBackendBase
 
 
-class MicrolabSTAR(HamiltonBackendBase): ...
+class MicrolabSTAR(HamiltonBackendBase):
+    def initialize(self):
+        if self.identifier in self._backend_instances:
+            plh_logger.critical(
+                f"Backend '{self.identifier}' has already been initialized. Initialization will be skipped and running instance will be maintained.",
+            )
+            return
+
+        plh_backend = PLHMicrolabSTAR(
+            identifier=self.identifier,
+            deck_layout=Path(self.deck_layout),
+            simulation_on=self.simulation_on,
+        )
+
+        self._backend_instances[self.identifier] = plh_backend
+
+        plh_backend.start()
+
+    def deinitialize(self):
+        if self.identifier not in self._backend_instances:
+            plh_logger.critical(
+                f"Backend '{self.identifier}' has not yet been initialized. Nothing will occur.",
+            )
+            return
+
+        plh_backend = self._backend_instances[self.identifier]
+
+        plh_backend.stop()
--- hal/backend/models/unchained_labs/stunner.py
+++ hal/backend/models/unchained_labs/stunner.py
@@ -1,4 +1,7 @@
 from django.db import models
+from plh.UnchainedLabs_Instruments.backend import Stunner as PLHStunner
+
+from debug import plh_logger
 
 from .unchained_labs_backend_base import UnchainedLabsBackendBase
 
@@ -6,3 +9,31 @@ from .unchained_labs_backend_base import UnchainedLabsBackendBase
 class Stunner(UnchainedLabsBackendBase):
     ip_address = models.CharField(max_length=50)
     port = models.SmallIntegerField()
+
+    def initialize(self):
+        if self.identifier in self._backend_instances:
+            plh_logger.critical(
+                f"Backend '{self.identifier}' has already been initialized. Initialization will be skipped and running instance will be maintained.",
+            )
+            return
+
+        plh_backend = PLHStunner(
+            identifier=self.identifier,
+            ip_address=self.ip_address,
+            port=self.port,
+        )
+
+        self._backend_instances[self.identifier] = plh_backend
+
+        plh_backend.start()
+
+    def deinitialize(self):
+        if self.identifier not in self._backend_instances:
+            plh_logger.critical(
+                f"Backend '{self.identifier}' has not yet been initialized. Nothing will occur.",
+            )
+            return
+
+        plh_backend = self._backend_instances[self.identifier]
+
+        plh_backend.stop()
