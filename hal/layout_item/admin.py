a9a6a77242d137cc6930c5d9e396ce59c3279a26
Bradley Bare <Bradley.Bare@pfizer.com>
2024-11-21 12:54:19 -0600

    Loaded layout item will be in advanced admin page

--- hal/layout_item/admin.py
+++ hal/layout_item/admin.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
+
 from hal.admin import hal_admin
 
-from .models import LayoutItemBase
+from .models import LoadedLayoutItem
 from .models.hamilton import HamiltonLayoutItem
 
+admin.site.register(LoadedLayoutItem)
 hal_admin.register(HamiltonLayoutItem)
