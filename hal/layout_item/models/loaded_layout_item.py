ba901a4eaed82c9c570cda7a62abdc2f153fd149
Bradley Bare <Bradley.Bare@pfizer.com>
2024-11-21 11:52:04 -0600

    New properties

--- hal/layout_item/models/loaded_layout_item.py
+++ hal/layout_item/models/loaded_layout_item.py
@@ -23,19 +23,51 @@ class LoadedLayoutItem(models.Model):
     )
 
     def __str__(self) -> str:
-        bottom_labware: list[str] = []
-        bottom = self.bottom
-        while bottom is not None:
-            bottom_labware.append(bottom.layout_item.labware.identifier)
-            bottom = bottom.bottom
+        bottom_labware_names = [
+            item.layout_item.labware.identifier for item in self.bottom_items
+        ]
+        top_labware_names = [
+            item.layout_item.labware.identifier for item in self.top_items
+        ]
 
-        top_labware: list[str] = []
+        return f"{' | '.join(bottom_labware_names)} * {self.layout_item.identifier} * {' | '.join(top_labware_names)}"
+
+    @property
+    def top_items(self) -> list[LoadedLayoutItem]:
+        """Return items above the current loaded layout item in ascending order."""
+        top_labware: list[LoadedLayoutItem] = []
         top = self.top
         while top is not None:
-            top_labware.append(top.layout_item.labware.identifier)
+            top_labware.append(top)
             top = top.top
 
-        bottom_labware.reverse()
-        # reverse because we are assembling from bottom up.
+        return top_labware
 
-        return f"{' | '.join(bottom_labware)} * {self.layout_item.identifier} * {' | '.join(top_labware)}"
+    @property
+    def absolute_top_item(self) -> LoadedLayoutItem:
+        return self.top_items[-1]
+
+    @property
+    def is_absolute_top_item(self) -> bool:
+        return self.top is None
+
+    @property
+    def bottom_items(self) -> list[LoadedLayoutItem]:
+        """Return items below the current loaded layout item in ascending order."""
+        bottom_labware: list[LoadedLayoutItem] = []
+        bottom = self.bottom
+
+        while bottom is not None:
+            bottom_labware.append(bottom)
+            bottom = bottom.bottom
+
+        return bottom_labware[::-1]
+        # reverse it so the order is ascending
+
+    @property
+    def absolute_bottom_item(self) -> LoadedLayoutItem:
+        return self.top_items[0]
+
+    @property
+    def is_absolute_bottom_item(self) -> bool:
+        return self.bottom is None
