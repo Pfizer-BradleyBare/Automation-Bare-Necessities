340927d84e760267cc9062e43ddee3331372df84
Bradley Bare <Bradley.Bare@pfizer.com>
2024-11-21 12:23:18 -0600

    Split file

--- hal/labware/models/__init__.py
+++ hal/labware/models/__init__.py
@@ -1,7 +1,8 @@
 from .container import Container
-from .labware_base import LabwareBase, StackedLabwareZHeightChange
+from .labware_base import LabwareBase
 from .non_pipettable_labware import NonPipettableLabware
 from .pipettable_labware import PipettableLabware
+from .stacked_labware_z_height_change import StackedLabwareZHeightChange
 
 __all__ = [
     "Container",
