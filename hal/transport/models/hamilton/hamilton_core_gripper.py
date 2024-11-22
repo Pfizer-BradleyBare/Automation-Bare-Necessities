bfa26a300057a83a3a8c34d97228221b02010e45
Bradley <bradley.bare@pfizer.com>
2024-11-22 10:31:02 -0600

    in progress

--- hal/transport/models/hamilton/hamilton_core_gripper.py
+++ hal/transport/models/hamilton/hamilton_core_gripper.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
-from typing import ClassVar
+from typing import ClassVar, cast
 
 from django.db import models
+from plh.hamilton_venus.HSLLabwrAccess import (
+    AbsolutePositionValuesGetForLabwareID,
+    AbsolutePositionValuesSetForLabwareID,
+)
 
 from hal.layout_item.models import LoadedLayoutItem
+from hal.layout_item.models.hamilton import HamiltonLayoutItem
 
 from ..transport_base import (
     TransportBase,
@@ -16,7 +21,7 @@ from ..transport_base import (
 class HamiltonCOREGripper(TransportBase):
     gripper_labware_id = models.CharField(max_length=100)
 
-    _max_grip_depth:ClassVar[float] = 40
+    _max_grip_depth: ClassVar[float] = 40
 
     class Meta:
         verbose_name = "Hamilton CORE Gripper"
@@ -29,10 +34,137 @@ class HamiltonCOREGripper(TransportBase):
     ) -> float:
         return 0
 
-    def transport(self, source: LoadedLayoutItem, destination: LoadedLayoutItem):
-        self.assert_transport(source,destination)
+    def assert_transport(self, source: LoadedLayoutItem, destination: LoadedLayoutItem):
+        _, grip_height = self.compute_grip_height(source)
 
-        grip_height = self.compute_long_side_grip_height(source)
+        if not isinstance(source.layout_item, HamiltonLayoutItem):
+            raise ValueError("Source is not a hamilton compatible layout item.")
+
+        if not isinstance(destination.layout_item, HamiltonLayoutItem):
+            raise ValueError("Destination is not a hamilton compatible layout item.")
+
+        if grip_height is None:
+            ValueError(
+                "No acceptable grip heights were found for the transport device '{self}'",
+            )
+
+        return super().assert_transport(source, destination)
+
+    def transport(self, source: LoadedLayoutItem, destination: LoadedLayoutItem):
+        from hal.carrier_location.models import TransportableCarrierLocationConfig
+
+        self.assert_transport(source, destination)
+
+        backend = self.backend.get_plh_backend()
+
+        source_labware_id = cast(
+            HamiltonLayoutItem,
+            source.layout_item,
+        ).venus_labware_id
+
+        destination_labware_id = cast(
+            HamiltonLayoutItem,
+            destination.layout_item,
+        ).venus_labware_id
+
+        command = AbsolutePositionValuesGetForLabwareID.Command(
+            options=[
+                AbsolutePositionValuesGetForLabwareID.Options(
+                    LabwareID=source_labware_id,
+                ),
+                AbsolutePositionValuesGetForLabwareID.Options(
+                    LabwareID=destination_labware_id,
+                ),
+            ],
+        )
+        backend.execute(command)
+        backend.wait(command)
+        response = backend.acknowledge(
+            command,
+            AbsolutePositionValuesGetForLabwareID.Response,
+        )
+        source_initial_position, destinition_initial_position = (
+            response.LabwarePositions
+        )
+        # get the current virtual positions so we can reset after transport
+
+        command = AbsolutePositionValuesSetForLabwareID.Command(
+            options=[
+                AbsolutePositionValuesSetForLabwareID.Options(
+                    LabwareID=source_labware_id,
+                    XPosition=source_initial_position.XPosition,
+                    YPosition=source_initial_position.YPosition,
+                    ZPosition=source_initial_position.ZPosition
+                    + source.z_height_above_base,
+                    ZRotation=source_initial_position.ZRotation,
+                ),
+            ],
+        )
+        backend.execute(command)
+        backend.wait(command)
+        backend.acknowledge(command, AbsolutePositionValuesSetForLabwareID.Response)
+
+        command = AbsolutePositionValuesSetForLabwareID.Command(
+            options=[
+                AbsolutePositionValuesSetForLabwareID.Options(
+                    LabwareID=destination_labware_id,
+                    XPosition=destinition_initial_position.XPosition,
+                    YPosition=destinition_initial_position.YPosition,
+                    ZPosition=destinition_initial_position.ZPosition
+                    + destination.height_from_base,
+                    ZRotation=destinition_initial_position.ZRotation,
+                ),
+            ],
+        )
+        backend.execute(command)
+        backend.wait(command)
+        backend.acknowledge(command, AbsolutePositionValuesSetForLabwareID.Response)
+        # move the labware virtually into the correct space first. Since labware can be stacked
+
+        source_config = TransportableCarrierLocationConfig.objects.filter(
+            transportablecarrierlocation=source.layout_item.carrier_location,
+            transport_device=self,
+        )
+
+        destination_config = TransportableCarrierLocationConfig.objects.filter(
+            transportablecarrierlocation=destination.layout_item.carrier_location,
+            transport_device=self,
+        )
+
+        _, grip_height = cast(tuple[None, float], self.compute_grip_height(source))
+
+        grip_labware = source.layout_item.labware
+
+        command = AbsolutePositionValuesSetForLabwareID.Command(
+            options=[
+                AbsolutePositionValuesSetForLabwareID.Options(
+                    LabwareID=source_labware_id,
+                    XPosition=source_initial_position.XPosition,
+                    YPosition=source_initial_position.YPosition,
+                    ZPosition=source_initial_position.ZPosition,
+                    ZRotation=source_initial_position.ZRotation,
+                ),
+            ],
+        )
+        backend.execute(command)
+        backend.wait(command)
+        backend.acknowledge(command, AbsolutePositionValuesSetForLabwareID.Response)
+
+        command = AbsolutePositionValuesSetForLabwareID.Command(
+            options=[
+                AbsolutePositionValuesSetForLabwareID.Options(
+                    LabwareID=destination_labware_id,
+                    XPosition=destinition_initial_position.XPosition,
+                    YPosition=destinition_initial_position.YPosition,
+                    ZPosition=destinition_initial_position.ZPosition,
+                    ZRotation=destinition_initial_position.ZRotation,
+                ),
+            ],
+        )
+        backend.execute(command)
+        backend.wait(command)
+        backend.acknowledge(command, AbsolutePositionValuesSetForLabwareID.Response)
+        # Reset the labware to the initial deck position.
 
 
 class HamiltonCOREGripperPickupOptions(TransportPickupOptionsBase):
