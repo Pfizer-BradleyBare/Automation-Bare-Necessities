fffb9b51f9d3dd3ebc3c3cab750905906d7a7cef
Pfizer-BradleyBare <Bradley.Bare@pfizer.com>
2024-11-21 20:13:43 -0600

    Moved methods in loaded_layout_item

--- hal/labware/models/stacked_labware_z_height_change.py
+++ hal/labware/models/stacked_labware_z_height_change.py
@@ -1,14 +1,8 @@
 from __future__ import annotations
 
-from itertools import pairwise
-from typing import TYPE_CHECKING
-
 from django.db import models
 
 from .labware_base import LabwareBase
-
-if TYPE_CHECKING:
-    from hal.layout_item.models import LoadedLayoutItem
 
 
 class StackedLabwareZHeightChange(models.Model):
@@ -27,72 +21,3 @@ class StackedLabwareZHeightChange(models.Model):
     z_height_change = models.FloatField(
         help_text="How much does the top labware nest into the bottom labware. Should always be equal to or less than 0. Ex. <stacked_labware_height> - (<top labware height> + <bottom labware height>)",
     )
-
-    @classmethod
-    def assert_supported_stack(cls, base: LoadedLayoutItem):
-        stack_pairs = list(
-            pairwise([item.layout_item.labware for item in base.top_items]),
-        )
-
-        q_query = models.Q()
-
-        for bottom, top in stack_pairs:
-            q_query |= models.Q(bottom_labware=bottom, top_labware=top)
-        # We do it this way first because a single query would be much faster.
-
-        if cls.objects.filter(q_query).count() != len(
-            stack_pairs,
-        ):
-            failed_stack_pairs: list[tuple[LabwareBase, LabwareBase]] = []
-
-            for bottom, top in stack_pairs:
-                if not cls.objects.filter(
-                    bottom_labware=bottom,
-                    top_labware=top,
-                ).exists():
-                    failed_stack_pairs.append((bottom, top))
-            # If it fails we do it this way because throwing an error is not time sensitive.
-
-            raise ValueError(
-                f"The following labware stack pairs are not supported: {failed_stack_pairs}",
-            )
-
-    @staticmethod
-    def compute_stack_z_dimension(base: LoadedLayoutItem) -> float:
-        labware_stack = [item.layout_item.labware for item in base.top_items]
-
-        labware_z_dimensions = [labware.height for labware in labware_stack]
-
-        nesting_z_heights = [
-            StackedLabwareZHeightChange.objects.filter(
-                bottom_labware=bottom,
-                top_labware=top,
-            )
-            .get()
-            .z_height_change
-            for bottom, top in pairwise(labware_stack)
-        ]
-
-        return sum(labware_z_dimensions + nesting_z_heights)
-
-    @staticmethod
-    def compute_stack_x_dimension(base: LoadedLayoutItem) -> float:
-        labware_stack = [item.layout_item.labware for item in base.top_items]
-
-        return max(
-            [
-                max([x for x, _, _ in labware.x_y_z_dimensions])
-                for labware in labware_stack
-            ],
-        )
-
-    @staticmethod
-    def compute_stack_y_dimension(base: LoadedLayoutItem) -> float:
-        labware_stack = [item.layout_item.labware for item in base.top_items]
-
-        return max(
-            [
-                max([y for y, _, _ in labware.x_y_z_dimensions])
-                for labware in labware_stack
-            ],
-        )
--- hal/layout_item/models/loaded_layout_item.py
+++ hal/layout_item/models/loaded_layout_item.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
+from itertools import pairwise
+
 from django.db import models
+
+from hal.labware.models import LabwareBase, StackedLabwareZHeightChange
 
 from .layout_item_base import LayoutItemBase
 
@@ -71,3 +75,68 @@ class LoadedLayoutItem(models.Model):
     @property
     def is_absolute_bottom_item(self) -> bool:
         return self.bottom is None
+
+    def assert_supported_stack(self):
+        stack_pairs = list(
+            pairwise([item.layout_item.labware for item in self.top_items]),
+        )
+
+        q_query = models.Q()
+
+        for bottom, top in stack_pairs:
+            q_query |= models.Q(bottom_labware=bottom, top_labware=top)
+        # We do it this way first because a single query would be much faster.
+
+        if StackedLabwareZHeightChange.objects.filter(q_query).count() != len(
+            stack_pairs,
+        ):
+            failed_stack_pairs: list[tuple[LabwareBase, LabwareBase]] = []
+
+            for bottom, top in stack_pairs:
+                if not StackedLabwareZHeightChange.objects.filter(
+                    bottom_labware=bottom,
+                    top_labware=top,
+                ).exists():
+                    failed_stack_pairs.append((bottom, top))
+            # If it fails we do it this way because throwing an error is not time sensitive.
+
+            raise ValueError(
+                f"The following labware stack pairs are not supported: {failed_stack_pairs}",
+            )
+
+    def compute_stack_z_dimension(self) -> float:
+        labware_stack = [item.layout_item.labware for item in self.top_items]
+
+        labware_z_dimensions = [labware.height for labware in labware_stack]
+
+        nesting_z_heights = [
+            StackedLabwareZHeightChange.objects.filter(
+                bottom_labware=bottom,
+                top_labware=top,
+            )
+            .get()
+            .z_height_change
+            for bottom, top in pairwise(labware_stack)
+        ]
+
+        return sum(labware_z_dimensions + nesting_z_heights)
+
+    def compute_stack_x_dimension(self) -> float:
+        labware_stack = [item.layout_item.labware for item in self.top_items]
+
+        return max(
+            [
+                max([x for x, _, _ in labware.x_y_z_dimensions])
+                for labware in labware_stack
+            ],
+        )
+
+    def compute_stack_y_dimension(self) -> float:
+        labware_stack = [item.layout_item.labware for item in self.top_items]
+
+        return max(
+            [
+                max([y for y, _, _ in labware.x_y_z_dimensions])
+                for labware in labware_stack
+            ],
+        )
